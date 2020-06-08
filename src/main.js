db381a94c0a52a62597f9ba645e8d4663978b4a9
layouwen <layouwen@gmail.com>
2020-06-08 10:37:50 +0800

    add html and js

---
+++ src/main.js
@@ -0,0 +1,53 @@
+// 基于准备好的dom，初始化echarts实例
+var myChart = echarts.init(document.getElementById('main'));
+
+// 指定图表的配置项和数据
+var option = {
+    radar: {
+        // shape: 'circle',
+        name: {
+            textStyle: {
+                color: '#fff',
+                backgroundColor: '#999',
+                borderRadius: 3,
+                padding: [3, 5]
+            }
+        },
+        indicator: [{
+                name: '静态页面',
+                max: 100
+            },
+            {
+                name: 'React 全家桶',
+                max: 100
+            },
+            {
+                name: 'Vue 全家桶',
+                max: 100
+            },
+            {
+                name: '沟通能力',
+                max: 100
+            },
+            {
+                name: '编程基础',
+                max: 100
+            },
+            {
+                name: '快速学习能力',
+                max: 100
+            }
+        ]
+    },
+    series: [{
+        name: '预算 vs 开销（Budget vs spending）',
+        type: 'radar',
+        // areaStyle: {normal: {}},
+        data: [{
+            value: [100, 75, 85, 70, 80, 90],
+        }, ]
+    }]
+};
+
+// 使用刚指定的配置项和数据显示图表。
+myChart.setOption(option);
